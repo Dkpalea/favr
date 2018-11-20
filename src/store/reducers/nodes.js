df6bf5505498dd8bce9f1535cf2e91e51393cb61
Dkpalea <dkpalea@ucsc.edu>
2018-11-20 14:47:21 -0800

    Combine react and web2py projects

--- src/store/reducers/nodes.js
+++
@@ -1,9 +0,0 @@
-export const initialState = { selectedNode: null };
-
-export function reducer(state = initialState, action) {
-  if (action.type === `selectNode`) {
-    console.log(`ok`);
-    return { ...state, selectedNode: action.nodeName };
-  }
-  return state;
-}
