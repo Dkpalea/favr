64c1022c0cd832d81577a3f4119f2ef21c7a3e72
azalahli <talesofxilliaftw@gmail.com>
2018-12-08 15:40:00 -0800

    had to refactor-pfp again. rip

--- applications/favr/react-files/src/components/ProfilePage.jsx
+++ applications/favr/react-files/src/components/ProfilePage.jsx
@@ -2,23 +2,30 @@ import React, {Component} from 'react';
 import Navbar from "./Navbar";
 import Feed from "./Feed";
 import { getFavr, getProfileInformation } from '../stateStoreAndFunctions';
+import { Redirect } from 'react-router';
 
 class ProfilePage extends Component {
   constructor(props) {
     super(props);
+    this.showFeedButton = this.showFeedButton.bind(this);
     this.state = {  feedFavrsState: storeState.feedFavrs,
                     requestedFavrState: storeState.myRequested,
                     profileSymbol: "\ud83d\ude01",
                     firstName: "mercy",
                     lastName: "pls",
+                    showFeed: false,
                  };
   }
 
   componentWillMount() {
-    getFavr(`myRequested`, this);
-    getFavr(`myAccepted`, this);
     getFavr(`feedFavr`, this);
     getProfileInformation();
+  }
+
+  showFeedButton(){
+    this.state.showFeed = !this.state.showFeed;
+    console.log(this.state.showFeed);
+    this.forceUpdate();
   }
 
 
@@ -34,6 +41,10 @@ class ProfilePage extends Component {
     })
     storeState.feedComponentHandle = this;
     // const now = new Date(Date.now() + 100000);
+    if (this.state.showFeed){
+        return <Redirect to="/profile/feed"></Redirect>;
+    }
+    else
     return (
       <div className="profile-page-container">
         <div className="profile-page-card">
@@ -45,9 +56,14 @@ class ProfilePage extends Component {
                 {this.state.lastName}
             </div>
         </div>
+        <button onClick={() => this.showFeedButton()} className="show-favr-feed">Show F&#257;vrs</button>
+
+{/*
         <div className="profile-feed-container">
           <Feed feedFavrs={this.state.feedFavrsState} mode="profile" />
         </div>
+*/}
+
       </div>
     );
   }
